871f950ead878f7a01f1da5389863621678611a4
vmred <vladdavidchenko@gmail.com>
2020-01-20 18:29:52 +0200

    add elements to droppable page

--- src/services/way2automation/pages/droppable_page.py
+++ src/services/way2automation/pages/droppable_page.py
@@ -1,3 +1,5 @@
+from selene.support.jquery_style_selectors import s
+
 from src.shared.ui.base_page import BasePage
 from variables import way2automation
 
@@ -5,4 +7,7 @@ from variables import way2automation
 class DroppablePage(BasePage):
     def __init__(self):
         super().__init__(f'{way2automation["hostname"]}{way2automation["droppable_page"]}')
-        self._page_content = 'div[class="container margin-top-20"]'
+        self._page_content = '//h1[contains(text(), "Droppable")]'
+
+    default_functionality_tab = s('//li[a[@href="#example-1-tab-1"]]')
+    accept_tab = s('//li[a[@href="#example-1-tab-2"]]')
