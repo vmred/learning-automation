d9a2cfb77fd2f9f889586b365631bb8df705a72e
vmred <vladdavidchenko@gmail.com>
2020-01-23 13:18:41 +0200

    add accept_tab_droppable_page fixture

--- tests/ui/test_way2automation/conftest.py
+++ tests/ui/test_way2automation/conftest.py
@@ -3,13 +3,12 @@ import pytest
 from src.services.way2automation.forms.registration import RegistrationForm
 from src.services.way2automation.pages.droppable_page import DroppablePage
 from src.services.way2automation.pages.home_page import HomePage
-from src.shared.ui.utils import wait_until_element_is_visible, log, wait_until_element_not_visible, \
-    is_element_displayed
+from src.shared.ui.utils import wait_until_element_is_visible, log, get_element_attributes
 
 
 @pytest.fixture
 def home_page():
-    return HomePage()
+    return HomePage().open()
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -20,14 +19,21 @@ def login():
     wait_until_element_is_visible(rf.form)
     log.info('registration form is opened')
     rf.login()
-    wait_until_element_not_visible(rf.login_form, timeout=4)
 
 
 @pytest.fixture
 def droppable_page(home_page):
-    dp = DroppablePage()
+    home_page.droppable_page_link.click()
+    return DroppablePage()
 
-    if not is_element_displayed(dp.page_content, timeout=0):
-        home_page.droppable_page_link.click()
 
-    return dp
+@pytest.fixture
+def accept_tab_droppable_page(droppable_page):
+    attrs = get_element_attributes(droppable_page.accept_tab)
+    if attrs and attrs.get('class') == 'active':
+        droppable_page.driver.refresh()
+        droppable_page.accept_tab.click()
+    else:
+        droppable_page.accept_tab.click()
+
+    return droppable_page
