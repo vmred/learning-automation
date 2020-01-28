a9ed5bd65bc3a5b00c07efb9adba93361acbf409
vmred <vladdavidchenko@gmail.com>
2020-01-28 18:30:02 +0200

    add capture fail fixture

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,8 +1,30 @@
+import allure
 import pytest
+from allure.constants import AttachmentType
 
 from src.shared.ui.cfg import browser
 
 driver = browser.driver()
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, 'rep_' + rep.when, rep)
+
+
+@pytest.fixture(scope='function', autouse=True)
+def capture_fail(request):
+    yield
+
+    if request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            allure.attach(request.function.__name__, driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        elif request.node.rep_call.passed:
+            allure.attach(request.function.__name__, driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+    elif request.node.rep_setup.failed:
+        allure.attach(request.function.__name__, driver.get_screenshot_as_png(), type=AttachmentType.PNG)
 
 
 @pytest.fixture(scope='session', autouse=True)
