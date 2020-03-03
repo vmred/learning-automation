1087b641f43e537ca1c76ec42964ec2f914415a0
vmred <vladdavidchenko@gmail.com>
2020-03-03 11:51:50 +0200

    update verify

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,22 @@ class TestCase:
     def fail(self):
         return self._fail
 
-    def verify(self, actual, expected, message=''):
-        if actual != expected:
-            self._fail += f'Actual result: {actual} not equal to expected: {expected}, message: {message or "Empty"} \n'
+    def verify(self, *args, reason=''):
+        # expects that args[0] == actual value
+        # args[1] == expected value
+
+        if len(args) == 1:
+            if not args[0]:
+                self._fail += 'condition mismatch'
+                if reason:
+                    self._fail += f', reason: {reason}\n'
+
+        elif args[0] != args[1]:
+            self._fail += f'actual value: {args[0]} is not equal to expected: {args[1]}'
+            if reason:
+                self._fail += f', reason: {reason}\n'
+
+        return self
 
     def is_failed(self):
         if self.fail:
